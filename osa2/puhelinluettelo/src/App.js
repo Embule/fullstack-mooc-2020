3f4150705c93b00e908c9ea51eac87441da5d399
Embule <koskinen.emily@gmail.com>
2020-08-03 20:38:38 +0300

    done exercise 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Person from './components/Person'
 import Form from './components/Form'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('add new person')
   const [newNumber, setNewNumber] = useState('add new number')
   const [newFilter, setNewFilter] = useState('')
+
+useEffect(() => {
+  axios
+  .get('http://localhost:3001/persons')
+  .then(res => {
+    setPersons(res.data)
+  })
+}, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -45,6 +49,8 @@ const App = () => {
   const filteredPersons = persons.filter(person => {
     return person.name.toLowerCase().includes(newFilter.toLowerCase())
   })
+
+  console.log(filteredPersons);
 
   return (
     <div>

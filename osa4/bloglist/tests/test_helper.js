5db02a463a59844f5c32d8e4f4ae7093d4e1eb55
Embule <koskinen.emily@gmail.com>
2022-02-02 15:06:46 +0200

    4.15 add POST request for creating users

--- osa4/bloglist/tests/test_helper.js
+++ osa4/bloglist/tests/test_helper.js
@@ -1,40 +1,46 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
-    {
-        // id: 1,
-        title: "01.12.2020",
-        author: "John Smith",
-        url: "www.facebook.com",
-        likes: 57
-    },
-    {
-        // id: 2,
-        title: "My Day",
-        author: "Katti Lallinen",
-        url: "www.github.com",
-        likes: 8
-    },
+  {
+    // id: 1,
+    title: "01.12.2020",
+    author: "John Smith",
+    url: "www.facebook.com",
+    likes: 57
+  },
+  {
+    // id: 2,
+    title: "My Day",
+    author: "Katti Lallinen",
+    url: "www.github.com",
+    likes: 8
+  },
 ]
 
 const nonExistingId = async () => {
-    const blog = new Blog({
-        title: "test",
-        author: "Nietzsche",
-        url: "www.facebook.com",
-        likes: 1
-    })
-    await blog.save()
-    await blog.remove()
+  const blog = new Blog({
+    title: "test",
+    author: "Nietzsche",
+    url: "www.facebook.com",
+    likes: 1
+  })
+  await blog.save()
+  await blog.remove()
 
-    return blog._id.toString()
+  return blog._id.toString()
 }
 
 const blogsInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(blog => blog.toJSON())
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
 }
 
 module.exports = {
-    initialBlogs, nonExistingId, blogsInDb
+  initialBlogs, nonExistingId, blogsInDb, usersInDb
 }
